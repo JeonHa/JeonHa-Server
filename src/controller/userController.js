56f108396904c8dad5788cdb5cc79c00a59fe7af
hyunjkluz <hyunjk.luz@gmail.com>
2019-09-19 18:30:12 +0900

    modify user signin api

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,15 +1,17 @@
 const { response, errResponse } = require('../library/response')
 const returnCode = require('../library/returnCode')
 
+const { sign } = require('../library/jwt');
 const userService = require('../service/userService');
 
 async function postUserSignup(req, res) {
     try {
-        const user = await userService.postUserSignup(req.body)
+        const user = await userService.postUserSignup(req.body);
+
+        response(res, returnCode.CREATED, '회원가입 성공', { 'authorization': sign(user.insertId) });
     } catch (error) {
         console.log(error.message);
         errResponse(res, returnCode.INTERNAL_SERVER_ERROR, '로그인 에러')
-
     }
 }
 
